bb3c1e0f151d26079731546ce8042adabf5a9c66
Matthew Phipps <mphipps@hubspot.com>
2019-09-04 00:19:05 -0400

    Trap SIGINT and SIGTERM

--- src/monitor-daemon.c
+++ src/monitor-daemon.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
 #include <unistd.h>
+
+void signal_handler(int signo) {
+    switch (signo) {
+        case SIGINT:
+        case SIGTERM:
+            printf("Goodbye world\n");
+            exit(128 + signo);
+    }
+}
 
 int main (int argc, const char * argv[]) {
     printf("Hello world!\n");
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+    printf("Signals ready\n");
     fflush(stdout);
     while (1) {
         sleep(1);
         printf("Doing a thing\n");
         fflush(stdout);
     };
-    printf("Goodbye world\n");
-    return 0;
 }
