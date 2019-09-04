7569806d8089e6cbc0911a2a2e4b05c3bcb23df1
Matthew Phipps <mphipps@hubspot.com>
2019-09-04 00:37:19 -0400

    Add event stream handler

--- src/monitor-daemon.c
+++ src/monitor-daemon.c
@@ -2,13 +2,17 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <unistd.h>
+#include <xpc/xpc.h>
+#include <notify_keys.h>
 
 void signal_handler(int signo) {
     switch (signo) {
         case SIGINT:
-        case SIGTERM:
             printf("Goodbye world\n");
             exit(128 + signo);
+        case SIGTERM:
+            printf("Goodbye world\n");
+            exit(0);
     }
 }
 
@@ -16,7 +20,12 @@ int main (int argc, const char * argv[]) {
     printf("Hello world!\n");
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
-    printf("Signals ready\n");
+    xpc_set_event_stream_handler("com.apple.notifyd.matching", 0, ^(xpc_object_t event) {
+                const char *name = xpc_dictionary_get_string(event, XPC_EVENT_KEY_NAME);
+                printf("%s\n", name);
+            }
+            );
+    printf("Initialized\n");
     fflush(stdout);
     while (1) {
         sleep(1);
