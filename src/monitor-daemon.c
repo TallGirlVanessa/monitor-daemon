77285ffdda065bb1b0b4a598190412a02132a9c2
Matthew Phipps <mphipps@hubspot.com>
2019-09-04 10:55:53 -0400

    Add datetime to logging

--- src/monitor-daemon.c
+++ src/monitor-daemon.c
@@ -4,14 +4,33 @@
 #include <unistd.h>
 #include <xpc/xpc.h>
 #include <notify_keys.h>
+#include <time.h>
+
+size_t DATETIME_LENGTH = strlen("2019-01-01 12:12:12");
+
+void time_for_log(char* out) {
+    time_t epoch_seconds;
+    struct tm calendar_time;
+    time(&epoch_seconds);
+    struct tm *result = gmtime_r(&epoch_seconds, &calendar_time);
+    if (result == 0) {
+        perror("gmtime_r error");
+        exit(1);
+    }
+    strftime(out, DATETIME_LENGTH, "%F %T", &calendar_time);
+}
 
 void signal_handler(int signo) {
+    char formatted_time[DATETIME_LENGTH + 1];
+    formatted_time[DATETIME_LENGTH] = 0;
     switch (signo) {
         case SIGINT:
-            printf("Goodbye world\n");
+            time_for_log(formatted_time);
+            printf("%s SIGINT, shutting down\n", formatted_time);
             exit(128 + signo);
         case SIGTERM:
-            printf("Goodbye world\n");
+            time_for_log(formatted_time);
+            printf("%s SIGTERM, shutting down\n", formatted_time);
             exit(0);
     }
 }
@@ -21,6 +40,10 @@ const int MAX_IP_LEN = 50;
 
 void get_SSID() {
     FILE *get_ssid_fp = popen("networksetup -getairportnetwork en0 | awk -F\": \" '{print $2}'", "r");
+    if (get_ssid_fp == 0) {
+        perror("popen error");
+        exit(1);
+    }
     char ssid[MAX_SSID_LEN];
     ssid[0] = 0;
     fgets(ssid, MAX_SSID_LEN, get_ssid_fp);
@@ -28,12 +51,17 @@ void get_SSID() {
     int status = pclose(get_ssid_fp);
     if (status == -1) {
         perror("pclose error");
+        exit(1);
     }
     printf("Current SSID: '%s'\n", ssid);
 }
 
 void get_network_info() {
     FILE *get_ip_fp = popen("networksetup -getinfo 'Wi-Fi' | grep 'IP address' | awk -F\": \" '{print $2}'", "r");
+    if (get_ip_fp == 0) {
+        perror("popen error");
+        exit(1);
+    }
     char ip[MAX_IP_LEN];
     ip[0] = 0;
     fgets(ip, MAX_IP_LEN, get_ip_fp);
@@ -51,7 +79,10 @@ void get_network_info() {
 }
 
 int main (int argc, const char * argv[]) {
-    printf("Hello world!\n");
+    char formatted_time[DATETIME_LENGTH + 1];
+    formatted_time[DATETIME_LENGTH] = 0;
+    time_for_log(formatted_time);
+    printf("%s Starting\n", formatted_time);
     signal(SIGINT, signal_handler);
     signal(SIGTERM, signal_handler);
     xpc_set_event_stream_handler("com.apple.notifyd.matching", 0, ^(xpc_object_t event) {
@@ -60,7 +91,8 @@ int main (int argc, const char * argv[]) {
             get_SSID();
             get_network_info();
             });
-    printf("Initialized\n");
+    time_for_log(formatted_time);
+    printf("%s Initialized\n", formatted_time);
     fflush(stdout);
     while (1) {
         sleep(1);
