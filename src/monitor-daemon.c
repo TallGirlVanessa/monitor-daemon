072504831053fa08a7c9a95b56d1477e5e565bd3
Matthew Phipps <mphipps@hubspot.com>
2019-09-04 00:08:59 -0400

    Flush prints

--- src/monitor-daemon.c
+++ src/monitor-daemon.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main (int argc, const char * argv[]) {
-    printf("Hello world!");
+    printf("Hello world!\n");
+    fflush(stdout);
+    while (1) {
+        sleep(1);
+        printf("Doing a thing\n");
+        fflush(stdout);
+    };
+    printf("Goodbye world\n");
     return 0;
 }
